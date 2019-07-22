9957875d3933d125d9566dc335880c4fbd3c0235
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-07-22 14:08:25 +0900

    Implement a factorial method.

--- chapter1/factorial.rb
+++ chapter1/factorial.rb
@@ -1,8 +1,30 @@
-def fact(n)
-  result = n
-  (n - 1).downto(0) do |i|
-    result += (result * i)
-  end
+def multi(a, b)
+  a * b
 end
 
-puts fact 2
+def fact(n)
+  return 1 if n <= 1
+
+  left = n
+  n.downto(2) do |i|
+    left = multi(left, i - 1)
+  end
+
+  return left
+end
+
+
+# testing
+require 'test/unit'
+
+class FactorialTest < Test::Unit::TestCase
+  def test_successful
+    assert_equal fact(0), 1
+    assert_equal fact(1), 1
+    assert_equal fact(2), 2
+    assert_equal fact(3), 6
+    assert_equal fact(4), 24
+    assert_equal fact(5), 120
+    assert_equal fact(9), 362_880
+  end
+end
